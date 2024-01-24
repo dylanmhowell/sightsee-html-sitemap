3bac8b57b380071a4e54c0f634e748cda8d78b99
Dylan Howell <34950771+dylanmhowell@users.noreply.github.com>
2024-01-24 11:29:17 -0800

    version 0.1.1

--- sightsee-html-sitemap.php
+++ sightsee-html-sitemap.php
@@ -4,7 +4,7 @@
  * Description:       An easy HTML Sitemap plugin created by the team at Sightsee Design.
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.1.0
+ * Version:           0.1.1
  * Author:            Sightsee Design
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
